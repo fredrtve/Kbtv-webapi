183217d534e5f921f3d0d7766002fc7c67ef40bc
Fredrik Tvete <fretve@hotmail.com>
2020-07-03 12:55:04 +0200

    add validation attr to delete timesheet command

--- src/BjBygg.Application/Commands/TimesheetCommands/Delete/DeleteTimesheetCommand.cs
+++ src/BjBygg.Application/Commands/TimesheetCommands/Delete/DeleteTimesheetCommand.cs
@@ -1,16 +1,16 @@
 using CleanArchitecture.Core.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace BjBygg.Application.Commands.TimesheetCommands.Delete
 {
     public class DeleteTimesheetCommand : IRequest<bool>
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Role { get; set; }
+        [Required]
         public int Id { get; set; }
     }
 }
