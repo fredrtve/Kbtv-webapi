ec4a7d3d7a0b1c48f3362a27b269f1f423088004
Fredrik Tvete <fretve@hotmail.com>
2020-07-19 21:31:00 +0200

    New mail template interface, removing old template data

---
+++ src/CleanArchitecture.Core/Interfaces/IMailTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Core.Interfaces
+{
+    public interface IMailTemplate<T>
+    {
+        string Key { get; }
+        T Data { get; set; }
+    }
+}
