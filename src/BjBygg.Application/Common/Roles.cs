26bbc85047f511fd99f5b849c15b93e9fb4339a9
Fredrik Tvete <fretve@hotmail.com>
2020-08-04 22:20:17 +0200

    add capital casing to roles

--- src/BjBygg.Application/Common/Roles.cs
+++ src/BjBygg.Application/Common/Roles.cs
@@ -4,10 +4,10 @@ namespace BjBygg.Application.Common
 {
     public static class Roles
     {
-        public const string Leader = "leder";
-        public const string Management = "mellomleder";
-        public const string Employee = "ansatt";
-        public const string Employer = "oppdragsgiver";
+        public const string Leader = "Leder";
+        public const string Management = "Mellomleder";
+        public const string Employee = "Ansatt";
+        public const string Employer = "Oppdragsgiver";
 
         public static readonly List<string> All = new List<string>() { Leader, Management, Employee, Employer };
     }
