8909af4c1933ac617b16e1d6907098e0bb5a84b8
Fredrik Tvete <fretve@hotmail.com>
2021-03-16 11:34:06 +0100

    use generic handler for create mission

--- src/BjBygg.Application/Application/Commands/MissionCommands/Create/CreateMissionCommandHandler.cs
+++ src/BjBygg.Application/Application/Commands/MissionCommands/Create/CreateMissionCommandHandler.cs
@@ -1,40 +1,14 @@
 using AutoMapper;
 using BjBygg.Application.Application.Common.Interfaces;
+using BjBygg.Application.Common.BaseEntityCommands.Create;
 using CleanArchitecture.Core.Entities;
-using MediatR;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace BjBygg.Application.Application.Commands.MissionCommands.Create
 {
-    public class CreateMissionCommandHandler : IRequestHandler<CreateMissionCommand>
+    public class CreateMissionCommandHandler : CreateCommandHandler<Mission, CreateMissionCommand>
     {
-        private readonly IAppDbContext _dbContext;
-        private readonly IMapper _mapper;
-        private readonly IBlobStorageService _storageService;
-        private readonly IImageResizer _imageResizer;
-
-        public CreateMissionCommandHandler(
-            IAppDbContext dbContext,
-            IMapper mapper,
-            IBlobStorageService storageService,
-            IImageResizer imageResizer)
-        {
-            _dbContext = dbContext;
-            _mapper = mapper;
-            _storageService = storageService;
-            _imageResizer = imageResizer;
-        }
-
-        public async Task<Unit> Handle(CreateMissionCommand request, CancellationToken cancellationToken)
-        {
-            var mission = _mapper.Map<Mission>(request);
-
-            await _dbContext.Set<Mission>().AddAsync(mission);
-
-            await _dbContext.SaveChangesAsync();
-
-            return Unit.Value;
-        }
+        public CreateMissionCommandHandler(IAppDbContext dbContext, IMapper mapper) :
+            base(dbContext, mapper)
+        { }
     }
 }
